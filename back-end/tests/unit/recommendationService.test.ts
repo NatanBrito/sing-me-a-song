32ddacbf07c6c8712acb80347b1dc678e671dbc0
Natan Brito <natanismaelbrito@gmail.com>
2022-07-29 15:17:58 -0300

    chore: done first endpoint verify unit tests

--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -1,17 +1,36 @@
 import { jest } from "@jest/globals";
+import { faker } from "@faker-js/faker";
 
 import { recommendationService } from "../../src/services/recommendationsService.js";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
 
 describe("unit test recommendation sucess", () => {
-  jest.spyOn(recommendationRepository, "create");
-  jest.spyOn(recommendationRepository, "findByName");
+  jest
+    .spyOn(recommendationRepository, "create")
+    .mockImplementation((): any => {});
 
-  it("insert recommendation sucess", () => {
-    // const insertService = recommendationService.insert({
-    //   name: "name teste",
-    //   youtubeLink: "https://www.youtube.com/watch?v=uaebgEChyDQ&t=2380s",
-    // });
-    expect("sagui").toEqual("sagui");
+  it("insert recommendation sucess", async () => {
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementation((): any => {
+        return false;
+      });
+    await recommendationService.insert({
+      name: faker.music.songName(),
+      youtubeLink: "https://www.youtube.com/watch?v=uaebgEChyDQ&t=23s",
+    });
+    expect(recommendationRepository.create).toBeCalled();
+  });
+  it("insert recommendation fail", async () => {
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockImplementation((): any => {
+        return true;
+      });
+    const insert = recommendationService.insert({
+      name: faker.music.songName(),
+      youtubeLink: "https://www.youtube.com/watch?v=uaebgEChyDQ&t=2380s",
+    });
+    expect(insert).rejects.toHaveProperty("type", "conflict");
   });
 });
