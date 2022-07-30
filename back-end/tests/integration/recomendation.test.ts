53edce73cd7eaa13b0705fc0aaef2171bd73091c
Natan Brito <natanismaelbrito@gmail.com>
2022-07-30 01:44:17 -0300

    chore: inicial integration tests done

--- back-end/tests/integration/recomendation.test.ts
+++ back-end/tests/integration/recomendation.test.ts
@@ -4,26 +4,60 @@ import app from "../../src/app.js";
 import * as dataRecomendation from "../scenarioFactorys/ScenarioRecomendation.js";
 import { prisma } from "../../src/database.js";
 
-beforeEach(async () => {
-  await prisma.recommendation.deleteMany();
-});
+// beforeEach(async () => {
+//   await prisma.recommendation.deleteMany();
+// });
 describe("recomendation tests", () => {
-  it(" test create /recommendations sucess", async () => {
+  it(" test create  sucess", async () => {
     const result = await supertest(app)
-      .post("/recommendations")
+      .post("/recommendations/")
       .send(dataRecomendation.CreateRecommendations());
     expect(result.status).toEqual(201);
   });
-  it(" upVote  recommendation", async () => {
-    // const users = await prisma.recommendation.findMany();
-    // const { id, score } = users[0];
-    // const result = await supertest(app)
-    //   .post(`/recommendations/${id}/upvote`)
-    //   .send();
-    // const CountVotesUpdate = await prisma.recommendation.findUnique({
-    //   where: { id: 1 },
-    // });
-    // expect(result.status).toEqual(200);
-    // expect(score).toBe(score);
+  it(" test get  sucess", async () => {
+    const result = await supertest(app).get("/recommendations/");
+    expect(result.status).toEqual(200);
+    expect(result.body).toBeDefined();
+  });
+  it(" upVote  sucess", async () => {
+    const { id, score } = await prisma.recommendation.findFirst({
+      where: { id: 1 },
+    });
+    const result = await supertest(app)
+      .post(`/recommendations/${id}/upvote`)
+      .send();
+    const upVote = await prisma.recommendation.findFirst({
+      where: { id: 1 },
+    });
+    expect(result.status).toEqual(200);
+    expect(score + 1).toEqual(upVote.score);
+  });
+  it(" donwVote  sucess", async () => {
+    const { id, score } = await prisma.recommendation.findFirst({
+      where: { id: 1 },
+    });
+    const result = await supertest(app)
+      .post(`/recommendations/${id}/downvote`)
+      .send();
+    const upVote = await prisma.recommendation.findFirst({
+      where: { id: 1 },
+    });
+    expect(result.status).toEqual(200);
+    expect(score - 1).toEqual(upVote.score);
+  });
+  it(" test random  sucess", async () => {
+    const result = await supertest(app).get("/recommendations/random");
+    expect(result.status).toEqual(200);
+    expect(result.body).toBeDefined();
+  });
+  it(" get amount  sucess", async () => {
+    const result = await supertest(app).get("/recommendations/top/100");
+    expect(result.status).toEqual(200);
+    expect(result.body).toBeDefined();
+  });
+  it(" get amount  sucess", async () => {
+    const result = await supertest(app).get("/recommendations/100");
+    expect(result.status).toEqual(200);
+    expect(result.body).toBeDefined();
   });
 });
