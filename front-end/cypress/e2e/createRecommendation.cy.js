86576c1f684d9abbc3d7b6cc41bd3d60d5f38783
Natan Brito <natanismaelbrito@gmail.com>
2022-08-01 08:19:23 -0300

    save first e2e

--- front-end/cypress/e2e/createRecommendation.cy.js
+++ front-end/cypress/e2e/createRecommendation.cy.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress" />
 import { faker } from "@faker-js/faker";
 describe("Recommendation", () => {
-  const youtubeLink = xx;
+  const nameSong = faker.music.songName();
+  const youtubeLink =
+    "https://www.youtube.com/watch?v=rs-cV16krbQ&list=RDrs-cV16krbQ&start_radio=1";
   it("create recommendation", () => {
     cy.visit("http://localhost:3000/");
-    cy.get("input[placeholder=Name]").type("natanzinho o malado");
-    cy.get("input[placeholder=https://youtu.be/...]").type(``);
+    cy.get("input[placeholder=Name]").type(nameSong);
+    cy.get("input[placeholder=https://youtu.be/...]").type(`${youtubeLink}`);
+    cy.get("#buttonRecommendation").click();
   });
 });
